e5b13e3e783bc655e52a2dcbe7fa637e80161a75
Joakim Saario <joakim.saario@truecaller.com>
2023-11-09 13:32:01 +0100

    wip: restoreiso

--- restoreiso/build.sh
+++ restoreiso/build.sh
@@ -5,28 +5,35 @@ set -e
 generate_ssh_key () {
     local ssh_passphrase=$1
 
+    local tmp
+    tmp=$(mktemp -d -t restoreiso-XXXX)
+    trap 'rm -rf $tmp' EXIT QUIT INT ERR
+
     keys_dir=airootfs/root/keys
     mkdir -p "$keys_dir"
 
-    ssh-keygen -t ed25519 -a 10000 -C restoreiso -N "$ssh_passphrase" \
-        -f "$keys_dir/remote-backup"
+    ssh-keygen -t ed25519 -C restoreiso -N "$ssh_passphrase" \
+        -f "$tmp/remote-backup"
+    printf "%s" "$ssh_passphrase" | obscura --argon2-time-cost=100 \
+        --argon2-parallellism=16 \
+        --argon2-memory-cost=4194304 \
+        "$tmp/remote-backup" > "$keys_dir/remote-backup.obscura"
 
     mkdir -p build
-    cp "$keys_dir"/remote-backup.pub build/restoreiso.pub
+    cp "$tmp/remote-backup.pub" build/restoreiso.pub
 }
 
 build_docker_image () {
-    local luks_passphrase=$1
-
-    docker build -t restoreiso-builder .
+    docker build --ulimit 'nofile=1024:1048576' -t restoreiso-builder .
 }
 
 build_iso () {
-    docker run --rm -v build:/mnt/build \
+    docker run --rm -v ./build:/mnt/build \
         --cap-add=SYS_CHROOT --cap-add=SYS_ADMIN \
         restoreiso-builder \
-        mkarchiso -v -w /tmp/archiso-tmp -o /mnt/build . '&&' \
-            chown "$(id -u):$(id -g)" /mnt/build/archlinux-*.iso
+        /bin/sh -c \
+        "mkarchiso -v -w /tmp/archiso-tmp -o /mnt/build . && \
+            chown $(id -u):$(id -g) /mnt/build/archlinux-*.iso"
 }
 
 passphrase_prompt () {
@@ -46,9 +53,10 @@ passphrase_prompt () {
 
 cd "$(dirname "$0")"
 
-ssh_passphrase=$(passphrase_prompt 'SSH key passphrase: ')
-luks_passphrase=$(passphrase_prompt 'Key store passphrase: ')
 
-generate_ssh_key "$ssh_passphrase"
-build_docker_image "$luks_passphrase"
+#ssh_passphrase=$(passphrase_prompt 'SSH key passphrase: ')
+
+#generate_ssh_key "$ssh_passphrase"
+
+build_docker_image
 build_iso
